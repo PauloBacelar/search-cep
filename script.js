42dbc8321932e1f6db97aff6a55682e9e091c19c
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-02-21 11:37:07 -0300

    Changed result div and added neighborhood

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Global variables
 const button = document.querySelector("button");
 const inputCEP = document.querySelector("input");
+const result = document.querySelector("div#result");
 const resultCEP = document.querySelector("h3#cep-result");
 const resultState = document.querySelector("h3#state-result");
 const resultCity = document.querySelector("h3#city-result");
+const resultNeighborhood = document.querySelector("h3#neighborhood-result");
 const resultStreet = document.querySelector("h3#street-result");
 const errorMessage = document.querySelector("div#error");
 
@@ -24,6 +26,20 @@ function cepToNum(cepString) {
   return Number(cepString.split("-").join(""));
 }
 
+function APIDataIsInvalid(apiData) {
+  return apiData.erro === true;
+}
+
+function showResults(apiData) {
+  result.classList.remove("hide");
+
+  resultCEP.textContent = `CEP: ${apiData.cep}`;
+  resultState.textContent = `State: ${apiData.uf}`;
+  resultCity.textContent = `City: ${apiData.localidade}`;
+  resultNeighborhood.textContent = `Neighborhood: ${apiData.bairro}`;
+  resultStreet.textContent = `Street: ${apiData.logradouro}`;
+}
+
 // Main function
 button.addEventListener("click", () => {
   const cepString = getInput();
@@ -40,6 +56,12 @@ button.addEventListener("click", () => {
       return jsonFile.json();
     })
     .then((apiData) => {
+      if (APIDataIsInvalid(apiData)) {
+        showErrorMessage();
+        return;
+      }
+
       console.log(apiData);
+      showResults(apiData);
     });
 });
